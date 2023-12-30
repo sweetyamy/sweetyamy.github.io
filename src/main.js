87778978936635224ad89a8d2b47436dfc81de04
Amy Ahn <eunmi.ahn@dcmail.ca>
2023-12-30 05:40:47 -0500

    JS-ArrowUp

--- src/main.js
+++ src/main.js
@@ -3,7 +3,8 @@
 // Header에 페이지 아래로 스크롤시 다크 스타일링 적용
 const header = document.querySelector('.header');
 const headerHeight = header.offsetHeight;
-document.addEventListener('scroll', () => {
+document.addEventListener('scroll', () => 
+{
   if (window.scrollY > headerHeight) {
     header.classList.add('header--dark');
   } else {
@@ -20,13 +21,28 @@ document.addEventListener('scroll', () => {
 
 // Arrow up버튼을 아래로 스크롤시 투명하게 처리함
 const arrowUp = document.querySelector('.arrow-up');
-document.addEventListener('scroll', () => {
-  if (window.scrollY > homeHeight / 2) {
-    arrowUp.style.opacity = 1;
-  } else {
-    arrowUp.style.opacity = 0;
-  }
+document.addEventListener('scroll', () => 
+{   
+    if (window.scrollY > homeHeight / 2) 
+    {
+        arrowUp.style.opacity = 1; 
+    }
+    else
+    {
+        arrowUp.style.opacity = 0; 
+    }
 });
+
+
+
+// const arrowUp = document.querySelector('.arrow-up');
+// document.addEventListener('scroll', () => {
+//   if (window.scrollY > homeHeight / 2) {
+//     arrowUp.style.opacity = 1;
+//   } else {
+//     arrowUp.style.opacity = 0;
+//   }
+// });
 
 // Navbar 토글버튼 클릭 처리
 const navbarMenu = document.querySelector('.header__menu');
